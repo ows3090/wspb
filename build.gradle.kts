5455f8a423c99ed5a71ecc4390e1449743087174
ows3090 <ows3090@naver.com>
2025-12-02 22:53:50 +0900

    Apply spotless for formatting kotlin code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.spotless) apply true
+}
+
+subprojects {
+    apply<SpotlessPlugin>()
+    configure<SpotlessExtension> {
+        kotlin {
+            // by default the target is every '.kt' and '.kts` file in the java sourcesets
+            ktfmt()    // has its own section below
+            ktlint()   // has its own section below
+            diktat()   // has its own section below
+            prettier() // has its own section below
+            // TODO : licenseHeader '/* (C)$YEAR */'
+        }
+        kotlinGradle {
+            target("*.gradle.kts") // default target for kotlinGradle
+            ktlint() // or ktfmt() or prettier()
+        }
+    }
 }
